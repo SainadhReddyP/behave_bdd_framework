2458d481e64a355496b0b60abc68827ac336bfbb
Sainadh Reddy <meetsainadhreddy@gmail.com>
2023-12-09 23:19:59 +0530

    added code for search feature

--- features/steps/search_steps.py
+++ features/steps/search_steps.py
@@ -1,36 +1,46 @@
 from behave import given, when, then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
 @given('user navigated to Home page')
-def step_impl(context):
-    pass
+def home_page(context):
+    context.driver = webdriver.Chrome()
+    context.driver.implicitly_wait(10)
+    context.driver.maximize_window()
+    context.driver.get("https://tutorialsninja.com/demo/")
 
 
 @when('user enter valid product into the search box')
-def step_impl(context):
-    pass
+def valid_search(context):
+    context.driver.find_element(By.NAME, "search").send_keys("HP")
 
 
 @when('user clicks on search button')
-def step_impl(context):
-    pass
-
-
-@when('user dont enter anything into search box')
-def step_impl(context):
-    pass
+def click_search(context):
+    context.driver.find_element(By.XPATH, "//div[@id='search']//button").click()
 
 
 @then('valid product should get displayed in search results')
-def step_impl(context):
-    pass
+def valid_search_result(context):
+    assert context.driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed()
+    context.driver.quit()
 
 
 @when('user enter invalid product into the search box')
-def step_impl(context):
-    pass
+def invalid_search(context):
+    context.driver.find_element(By.NAME, "search").send_keys("DELL")
 
 
 @then('proper message should be displayed in search results')
-def step_impl(context):
-    pass
+def message_invalid_search(context):
+    expected_text = "There is no product that matches the search criteria."
+    assert context.driver.find_element(By.XPATH, "//input[@id='button-search']/following-sibling::p")\
+        .text.__eq__(expected_text)
+    context.driver.quit()
+
+
+@when('user dont enter anything into search box')
+def empty_search(context):
+    context.driver.find_element(By.NAME, "search").send_keys("")
