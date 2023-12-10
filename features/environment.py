8c40396f7d24cbe682bec2cb8a9517b83e464055
Sainadh Reddy <meetsainadhreddy@gmail.com>
2023-12-10 13:35:57 +0530

    issue fix in environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 
 
-def before_scenario(context):
+def before_scenario(context, driver):
     context.driver = webdriver.Chrome()
     context.driver.maximize_window()
     context.driver.get("https://tutorialsninja.com/demo/")
 
 
-def after_scenario(context):
+def after_scenario(context, driver):
     context.driver.quit()
