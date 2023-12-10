769d0fdc76cfb237cb5a7b028c7094ef8b8b5cf2
Sainadh Reddy <meetsainadhreddy@gmail.com>
2023-12-10 13:51:19 +0530

    added configurations and config.ini added browsers utility to environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from utilities import ConfigReader
 
 
 def before_scenario(context, driver):
-    context.driver = webdriver.Chrome()
+    browser_name = ConfigReader.read_configuration("basic_info", "browser")
+    if browser_name.__eq__("chrome"):
+        context.driver = webdriver.Chrome()
+    elif browser_name.__eq__("firefox"):
+        context.driver = webdriver.Firefox()
+    elif browser_name.__eq__("edge"):
+        context.driver = webdriver.Edge()
+
     context.driver.maximize_window()
-    context.driver.get("https://tutorialsninja.com/demo/")
+    context.driver.get(ConfigReader.read_configuration("basic_info", "url"))
 
 
 def after_scenario(context, driver):
